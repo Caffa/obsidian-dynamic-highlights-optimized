f62b07801ce4f018c658661d1c8ba6e76162691f
caffae <wangyeelin@gmail.com>
2025-06-20 00:13:33 +0800

    Prepare for Git Release. Bump version to 0.2.3

--- prepareForGitRelease.sh
+++ prepareForGitRelease.sh
@@ -1,13 +1,12 @@
 # minor version bump
-npm version patch
+npm version patch --no-git-tag-version
 
 # create the current_release directory if it does not exist
 mkdir -p dynamic-highlights-optimized
 
 # make a copy of the main.js, manifest.json, and styles.css files in another folder
-cp main.js dynamic-highlights-optimized
+cp dist/main.js dynamic-highlights-optimized
 cp manifest.json dynamic-highlights-optimized
-cp styles.css dynamic-highlights-optimized
 
 # compress the current_release folder into a zip file
 # zip -r release.zip current_release
@@ -18,12 +17,14 @@ mv dynamic-highlights-optimized.zip release.zip
 # remove the current_release folder
 # rm -rf dynamic-highlights-optimized
 
-# gac "Push for Release"
+# Get the new version and create a tag without 'v' prefix
+VERSION=$(node -p "require('./package.json').version")
 git add -A
-git commit -m "Prepare for Git Release"
+git commit -m "Prepare for Git Release. Bump version to $VERSION"
+git tag $VERSION
 # git push origin main
 echo "Pushing to main tag... "
-echo "git push origin tag 1.0.4"
+echo "git push origin tag $VERSION"
 echo "Creating a new release... "
-echo 'gh release create 1.0.4 release.zip main.js manifest.json styles.css --title "Aesthetics" --notes "Improve aesthetics"'
+echo "gh release create $VERSION release.zip main.js manifest.json styles.css --title \"Aesthetics\" --notes \"Improve aesthetics\""
 
